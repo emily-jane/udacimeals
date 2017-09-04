b5e5e11b1383c031a89bd4ec60bfa373ee29f352
Emily Carson <Macbookpro@Macbooks-MBP.lan>
2017-09-04 16:05:08 +0100

    added logging middleware

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { connect } from 'react-redux';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers'
+import { Provider } from 'react-redux'
+import logger from 'redux-logger'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(logger)
+  )
 )
 
 ReactDOM.render(
@@ -18,5 +22,5 @@ ReactDOM.render(
     <App />
   </Provider>,
   document.getElementById('root')
-);
-registerServiceWorker();
+)
+registerServiceWorker()
